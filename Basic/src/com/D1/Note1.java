f70b78d6b6f6021378ffd363ed4d3528a9285796
McQueen979 <454239295@qq.com>
2025-07-15 07:07:10 +0800

    Java study notes

---
+++ Basic/src/com/D1/Note1.java
@@ -0,0 +1,18 @@
+package com.D1;
+
+public class Note1 {
+    public static void main(String[] args) {
+        System.out.println("让她杀了你！！！");
+    }
+}
+
+/*
+    项目project  模块module  包package  类class
+
+    数据类型：
+
+
+
+
+
+ */
